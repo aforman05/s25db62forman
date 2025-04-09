1bfefc4dc970516ad5d98d2fe013f5546e47e72e
Andrea F <143133024+aforman05@users.noreply.github.com>
2025-04-09 18:42:05 -0500

    Updated project to show all in the pug view and created the post function to create a new instance of the collection

--- controllers/location.js
+++ controllers/location.js
@@ -2,12 +2,24 @@ var Location = require('../models/location');
 // List of all Costumes
 exports.location_list = async function(req, res) {
     try{
-    theLocations = await Location.find();
-    res.send(theLocations);
+        theLocations = await Location.find();
+        res.send(theLocations);
     }
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+// VIEWS
+// Handle a show all view
+exports.location_view_all_Page = async function(req, res) {
+    try{
+        theLocations = await Location.find();
+        res.render('locations', { title: 'Location Search Results', results: theLocations });
+    }
+    catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
 };
 // for a specific Costume.
@@ -15,8 +27,24 @@ exports.location_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Location detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.location_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Location create POST');
+exports.location_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Location();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.longitude = req.body.longitude;
+    document.latitude = req.body.latitude;
+    document.climate = req.body.climate;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
 };
 // Handle Costume delete from on DELETE.
 exports.location_delete = function(req, res) {
--- routes/locations.js
+++ routes/locations.js
@@ -1,9 +1,7 @@
 var express = require('express');
+const location_controllers= require('../controllers/location');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('locations', { title: 'Search Results Locations' });
-});
-
+/* GET locations page. */
+router.get('/', location_controllers.location_view_all_Page );
 module.exports = router;
