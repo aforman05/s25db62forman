f860c451130da6439385801ed02c340402cdec86
Andrea F <143133024+aforman05@users.noreply.github.com>
2025-04-16 23:46:11 -0500

    Created new router, view, and controller to be able to create a new location and save it.

--- routes/locations.js
+++ routes/locations.js
@@ -6,9 +6,6 @@ var router = express.Router();
 router.get('/', location_controllers.location_view_all_Page );
 module.exports = router;
 
-// GET request for a location.
-router.get('/:id', location_controllers.location_detail);
-
 //Post request for a location
 router.post('/:id', location_controllers.location_update_put);
 
@@ -17,3 +14,9 @@ router.delete('/:id', location_controllers.location_delete);
 
 //Get detail location page
 router.get('/detail/:id', location_controllers.location_view_one_Page);
+
+//Get create location page
+router.get('/create', location_controllers.location_create_Page);
+
+// GET request for a location.
+router.get('/:id', location_controllers.location_detail);
