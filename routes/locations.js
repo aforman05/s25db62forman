f8b763ca512e867fa1473f7954ef4afbecc6a9e5
Andrea F <143133024+aforman05@users.noreply.github.com>
2025-04-23 02:56:33 -0500

    Protected the update endpoint

--- routes/locations.js
+++ routes/locations.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const location_controllers= require('../controllers/location');
 var router = express.Router();
+var passport = require('passport');
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 /* GET locations page. */
 router.get('/', location_controllers.location_view_all_Page );
@@ -19,7 +29,11 @@ router.get('/detail/:id', location_controllers.location_view_one_Page);
 router.get('/create', location_controllers.location_create_Page);
 
 /* GET create update page */
-router.get('/update/:id', location_controllers.location_update_Page);
+router.get('/update/:id', secured, location_controllers.location_update_Page);
 
 // GET request for a location.
 router.get('/:id', location_controllers.location_detail);
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
